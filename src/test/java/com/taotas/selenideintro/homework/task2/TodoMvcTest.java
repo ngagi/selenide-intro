64298ce2dc84ec0ed8b5bde6deb53bf4e7c6f02a
natalialukmanova <nlukmanova@bekitzur.com>
2020-06-15 22:23:27 +0300

    homework2, version1

---
+++ src/test/java/com/taotas/selenideintro/homework/task2/TodoMvcTest.java
@@ -0,0 +1,26 @@
+package com.taotas.selenideintro.homework.task2;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TodoMvcTest {
+    @Test
+    void completesTask() {
+        open("https://todomvc.com/examples/emberjs/");
+
+        element(byXpath("//*[@id='new-todo']")).setValue("a").pressEnter();
+        element(byXpath("//*[@id='new-todo']")).setValue("b").pressEnter();
+        element(byXpath("//*[@id='new-todo']")).setValue("c").pressEnter();
+        elements(byXpath("//*[@id='todo-list']/li")).shouldHave(exactTexts("a", "b", "c"));
+
+        element(byXpath("//*[@id='todo-list']/li[2]//*[@class='toggle']")).click();
+
+        elements(byXpath("//*[@id='todo-list']/li[contains(concat(' ', normalize-space(@class), ' '), ' completed ')]"))
+                .shouldHave(exactTexts("b"));
+        elements(byXpath("//*[@id='todo-list']/li[not(contains(concat(' ', normalize-space(@class), ' '), ' completed '))]"))
+                .shouldHave(exactTexts("a", "c"));
+    }
+}
